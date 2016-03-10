8f02074a8e9e928674439a64584dfd7e7f999a49
조성용 <niegel0128@gmail.com>
2016-03-10 11:12:30 +0900

    java statement 프로젝트 시작 !!

---
+++ javaStmt/src/forloop/Oddsum.java
@@ -0,0 +1,17 @@
+package forloop;
+
+/**
+ * @file Oddsum.java
+ * @author CSY
+ * @date 2016. 3. 10.
+ * @story
+ */
+public class Oddsum {
+	public static void main(String[] args){
+		int sum = 0;
+		for(int i = 1;i<=10;i+=2){
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+}
